adaf2d9e677819bb5b7cccdb89ea0357a802bcb9
Joaqu1nMS <joaquin.martin.ms@gmail.com>
2026-05-12 19:15:50 +0200

    Object picker
    Ya se pueden recoger objetos. De momento son eliminados sin mas. Mas adelante habra que hacer que lo elimine de la lista de objetivos

---
+++ P2 - PJ/Assets/Scripts/ObjectPicker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPicker : MonoBehaviour
+{
+    private Outline lineado;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float alcanceRecogida = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineado = gameObject.GetComponent<Outline>();
+        lineado.enabled = false;
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {        
+        if (Vector3.Distance(transform.position, player.transform.position) <= alcanceRecogida)
+        {
+            lineado.enabled = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("ObjetoRecogido");
+                Destroy(this.gameObject);
+            }    
+        } else
+        {
+            lineado.enabled = false;
+        }
+        
+    }
+
+    private void OnMouseExit()
+    {        
+        lineado.enabled = false;            
+    }
+}
