8431319c4b497e4920f330df09eef87719ea8982
Joaqu1nMS <joaquin.martin.ms@gmail.com>
2026-05-16 18:06:00 +0200

    Object picker funcional para un nivel

---
+++ P2 - PJ/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class ObjectManager : MonoBehaviour
+{
+    List<GameObject> pickeables = new List<GameObject>();
+    List<GameObject> objetosTXT = new List<GameObject>();
+
+    [Header("Objetos robables")]
+    public int cantidadActivos = 3;
+
+    [Header("Referencias")]
+    public GameObject canvasLista;
+    private bool canvasActivo = false;
+    public GameObject notas;
+    public GameObject prefabTexto;
+
+    void Start()
+    {
+        // Obtener todos los ObjectPicker
+        foreach (ObjectPicker obj in FindObjectsOfType<ObjectPicker>())
+        {
+            pickeables.Add(obj.gameObject);
+        }
+        canvasLista.SetActive(canvasActivo);
+        SeleccionarObjetos();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!canvasActivo) canvasActivo = true;
+            else canvasActivo = false;
+            canvasLista.SetActive(canvasActivo);
+        }
+    }
+
+    void SeleccionarObjetos()
+    {
+        // Mezclar lista aleatoriamente
+        for (int i = 0; i < pickeables.Count; i++)
+        {
+            int randomIndex = Random.Range(i, pickeables.Count);
+
+            GameObject temp = pickeables[i];
+            pickeables[i] = pickeables[randomIndex];
+            pickeables[randomIndex] = temp;
+        }
+
+        // Desactivar ObjectPicker del resto
+        for (int i = cantidadActivos; i < pickeables.Count; i++)
+        {
+            ObjectPicker op = pickeables[i].GetComponent<ObjectPicker>();
+            Outline ou = pickeables[i].GetComponent<Outline>();
+
+            if (op != null)
+            {
+                op.enabled = false;                
+                ou.enabled = false;
+            }
+        }
+
+        foreach (GameObject activo in pickeables.Where(x => x.GetComponent<ObjectPicker>().enabled))
+        {
+            GameObject txt = Instantiate(prefabTexto, Vector2.zero, Quaternion.identity, notas.transform);
+            txt.GetComponent<TextMeshProUGUI>().text = activo.GetComponent<ObjectPicker>().nombreDescripcion;
+            objetosTXT.Add(txt);
+        }
+    }
+
+    public void HeSidoRecogido(string text)
+    {
+        GameObject obj = objetosTXT.FirstOrDefault(x => x.GetComponent<TextMeshProUGUI>().text.Equals(text));
+        if (obj != null)
+        {
+            obj.GetComponent<TextMeshProUGUI>().fontStyle = TMPro.FontStyles.Strikethrough;
+        }
+    }
+}
--- P2 - PJ/Assets/Scripts/ObjectPicker.cs
+++ P2 - PJ/Assets/Scripts/ObjectPicker.cs
@@ -7,6 +7,7 @@ public class ObjectPicker : MonoBehaviour
     private Outline lineado;
     [SerializeField] private GameObject player;
     [SerializeField] private float alcanceRecogida = 2f;
+    public string nombreDescripcion;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +20,14 @@ public class ObjectPicker : MonoBehaviour
     // Update is called once per frame
     private void OnMouseOver()
     {        
+        if (!enabled) return;
         if (Vector3.Distance(transform.position, player.transform.position) <= alcanceRecogida)
         {
             lineado.enabled = true;
             if (Input.GetKey(KeyCode.E))
             {
                 Debug.Log("ObjetoRecogido");
+                FindObjectOfType<ObjectManager>().HeSidoRecogido(nombreDescripcion);
                 Destroy(this.gameObject);
             }    
         } else
@@ -36,6 +39,7 @@ public class ObjectPicker : MonoBehaviour
 
     private void OnMouseExit()
     {        
+        if (!enabled) return;
         lineado.enabled = false;            
     }
 }
